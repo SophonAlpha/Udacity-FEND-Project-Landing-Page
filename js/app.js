d3ad6efeb87565549caf781f170f4f867d4b1199
SophonAlpha <SophonAlpha@users.noreply.github.com>
2021-06-27 23:12:19 +0400

    feat: rework underline active menu item

--- js/app.js
+++ js/app.js
@@ -1,55 +1,107 @@
-let activeMenuItemText = null;
+class Menu {
+
+    constructor(menuIds) {
+        // this.instance = this;
+        this.activeMenuItems = [];
+        this.initialise(menuIds);
+    }
+
+    initialise(menuIds) {
+        for (let menuId of menuIds) {
+            const menu = document.getElementById(menuId);
+            this.activeMenuItems.push(menu.children[0]);
+        }
+        // Underline first menu entry.
+        this.setUnderline(this.activeMenuItems);
+    }
+
+    menuItemClick(event) {
+        let section = null;
+        let menuItemIds = null
+        let elem = event.target;
+        while (!(elem === null) && !(elem.tagName === "BODY")) {
+            if (elem.hasAttribute('data-section-id')) {
+                section = elem.getAttribute('data-section-id');
+                menuItemIds = elem.getAttribute('data-menu-items');
+                break;
+            }
+            elem = elem.parentNode;
+        }
+        console.log("Section = " + section);
+        console.log("Menu items = " + this.activeMenuItems);
+    }
+
+
+    // menuItemClick(menuItem) {
+    //     // const menuItem = document.getElementById(menuItemId);
+    //     const section = document.getElementById(
+    //         menuItem.getAttribute('data-section-id')
+    //     );
+    //     section.scrollIntoView({behavior: "smooth"});
+    //     this.setUnderline(menuItem);
+    // }
+
+    setUnderline(selectedMenuItems) {
+        for (let selectedMenuItem of selectedMenuItems) {
+            const menuItemText = document.getElementById(selectedMenuItem.id + "-text");
+            // Remove underline from active menu item.
+            if (this.activeMenuItems) {
+                if (this.activeMenuItems.classList.contains("underlined")) {
+                    this.activeMenuItems.classList.remove("underlined");
+                }
+                if (this.activeMenuItems.classList.contains("underline-anchor")) {
+                    this.activeMenuItems.classList.remove("underline-anchor");
+                }
+            }
+            // Add underline to selected menu item.
+            menuItemText.classList.add("underlined");
+            menuItemText.classList.add("underline-anchor");
+        }
+        // Save selected item as active item.
+        this.activeMenuItems = selectedMenuItems;
+    }
+
+}
 
 function main() {
-    // Attach event function to all menu items.
-    for (let menuId of ['menu-horiz', 'menu-vert']) {
-        const menu = document.getElementById(menuId);
-        // Underline first menu entry.
-        setUnderline(menu.children[0]);
-        for (let menuItem of menu.children) {
-            menuItem.addEventListener('click', function() {
-                menuItemClick(menuItem.id);
-            });
-        }
-    }
+    // Initialise horizontal and vertical menus.
+    const menus = new Menu(['menu-horiz', 'menu-vert']);
+    document.getElementById("header-box")
+        .addEventListener('click', menus.menuItemClick.bind(menus));
     // Attach event function to menu icon (visible on narrow screens).
-    document.getElementById('menu-icon').addEventListener('click', menuIconClick);
+    document.getElementById('menu-icon')
+        .addEventListener('click', menuIconClick);
     // Respond to media queries for window resizing.
-    window.matchMedia('(min-width: 950px)').addEventListener('change', toggleVerticalMenu);
+    window.matchMedia('(min-width: 950px)')
+        .addEventListener('change', toggleVerticalMenu);
     toggleVerticalMenu();
     // Scroll to top section.
     const sections = document.getElementById("sections");
     sections.scrollIntoView();
 }
 
-function setUnderline(menuItem) {
-    const menuItemText = document.getElementById(menuItem.id + "-text");
-    // Remove underline from current menu item.
-    if (activeMenuItemText) {
-        if (activeMenuItemText.classList.contains("underlined")) {
-            activeMenuItemText.classList.remove("underlined");
-        }
-        if (activeMenuItemText.classList.contains("underline-anchor")) {
-            activeMenuItemText.classList.remove("underline-anchor");
-        }
-    }
-    // Add underline to active menu item.
-    menuItemText.classList.add("underlined");
-    console.log(menuItemText);
-    menuItemText.classList.add("underline-anchor");
-    // Save active item as current item.
-    activeMenuItemText = menuItemText;
-}
-
-function menuItemClick(menuItemId) {
-    console.log(menuItemId);
-    const section = document.getElementById(
-        document
-            .getElementById(menuItemId)
-            .getAttribute('data-section-id')
-    );
-    section.scrollIntoView({behavior: "smooth"});
-}
+// function initMenus(menuHorizId, menuVertId) {
+//     const menuHoriz = document.getElementById(menuHorizId);
+//     const menuVert = document.getElementById(menuVertId);
+//     const mapHorizToVert = {};
+//     const mapVertToHoriz = {};
+//     for (let i = 0; i < menuHoriz.children.length; i++) {
+//         mapHorizToVert[menuHoriz.children[i].id] = menuVert.children[i];
+//         mapVertToHoriz[menuVert.children[i].id] = menuHoriz.children[i];
+//     }
+//     mapHorizToVert["active"] =
+//
+//         // Underline first menu entry.
+//         this.setUnderline(menu.children[0]);
+//         // Attach event handler.
+//         for (let menuItem of menu.children) {
+//             const instance = this;
+//             menuItem.addEventListener('click', function () {
+//                 instance.menuItemClick(menuItem);
+//             });
+//         }
+//
+// }
 
 function toggleVerticalMenu() {
     // Toggle the vertical menu whenever a media query resize event fires.

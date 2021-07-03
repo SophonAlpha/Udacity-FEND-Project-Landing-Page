599cfdc21434088e6914be0a25a08f5870e67712
SophonAlpha <SophonAlpha@users.noreply.github.com>
2021-07-03 22:43:08 +0400

    refactor: Simplified code. No longer two menus (horizontal and vertical).

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let state = {
-    activeMenuItems: [],
+    activeMenuItem: [],
     lastScrollPos: 0,
     isScrolling: null,
     sectionsElem: null,
@@ -8,9 +8,9 @@ let state = {
 
 function main() {
 
-    // Initialise horizontal and vertical menus.
-    menuInitialise(['menu-horiz', 'menu-vert']);
-    document.getElementById("header-box")
+    // Build horizontal and vertical menus based on sections in the document.
+    buildMenu();
+    document.getElementById('header-box')
         .addEventListener('click', menuItemClick);
 
     // Attach event function to menu icon (visible on narrow screens).
@@ -18,100 +18,135 @@ function main() {
         .addEventListener('click', menuIconClick);
 
     // Attach scroll event handler.
-    state.sectionsElem = document.getElementById("sections");
-    state.sectionsElem.addEventListener("scroll", scroll);
+    state.sectionsElem = document.getElementById('sections');
+    state.sectionsElem.addEventListener('scroll', scroll);
 
     // Set up response to media queries for window resizing.
-    window.matchMedia('(min-width: 950px)')
-        .addEventListener('change', toggleVerticalMenu);
-    toggleVerticalMenu();
+    //
+    // Note: The menu items are dynamically added based on the sections in the HTML page.
+    // We need to wait for the browser reflow and repaint to complete before we can query
+    // the width of the menu bar. If we don't do this .getBoundingClientRect() will report
+    // 0's for widths and heights
+    window.requestAnimationFrame(updateMediaQuery);
 
     // Scroll to top section.
-    const sections = document.getElementById("sections");
+    const sections = document.getElementById('sections');
     sections.scrollIntoView();
 }
 
-function menuInitialise(menuIds) {
-    let menuItems = [];
-    for (let menuId of menuIds) {
-        const menu = document.getElementById(menuId);
-        menuItems.push(menu.children[0]);
+function buildMenu() {
+    const menuId = 'menu';
+    // Get all sections.
+    const sections = document.getElementsByClassName('section');
+    // Add sections to menu.
+    for (let section of sections) {
+        let sectionId = section.id
+        let menuText = section.getAttribute('data-menu-item-text');
+        let elem = document.createElement('DIV');
+        // Add menu item to horizontal and vertical menus.
+        elem.innerHTML =
+            `<li id="${menuId}-${sectionId}" data-section-id="${sectionId}" ` +
+            `class="header__menu-item"> ` +
+            `<span id="${menuId}-${sectionId}-text">${menuText}</span>` +
+            `</li>`;
+        let menuItem = elem.firstChild;
+        document.getElementById(menuId).appendChild(menuItem);
     }
-    // Underline first menu entry in horizontal and vertical menus.
-    menuSetUnderline(menuItems);
+    // Underline first menu entry.
+    menuSetUnderline(document.getElementById(menuId).children[0]);
+}
+
+function updateMediaQuery() {
+    // console.log(document.getElementById('menu').getBoundingClientRect().width);
+    // console.log(document.getElementById('header__logo').getBoundingClientRect().width);
+    // const triggerWidth = document.getElementById('menu')
+    //         .getBoundingClientRect().width +
+    //     document.getElementById('header__logo')
+    //         .getBoundingClientRect().width + 70;
+    // console.log('triggerWidth = ' + triggerWidth);
+    // // Add media rule.
+    // const mediaRule = '' +
+    //     `@media screen and (min-width: ${triggerWidth}px) {` +
+    //     '  .header__menu {' +
+    //     '    visibility: visible;' +
+    //     '  }' +
+    //     '  .header__menu-icon {' +
+    //     '    display: none;' +
+    //     '  }' +
+    //     '}';
+    // const style = document.createElement('style');
+    // document.head.appendChild(style);
+    // style.sheet.insertRule(mediaRule);
+    // Attache event handler to toggle horizontal or vertical menu.
+    window.matchMedia(`(min-width: ${950}px)`)
+        .addEventListener('change', toggleVerticalMenu);
+    toggleVerticalMenu();
 }
 
 function menuItemClick(event) {
     let sectionId = null;
-    let menuItemIds = null;
+    let menuItem = null;
     let elem = event.target;
-    let menuItems = [];
 
     // Traverse up the DOM tree to find the menu item clicked.
-    while (!(elem === null) && !(elem.tagName === "BODY")) {
+    while (!(elem === null) && !(elem.tagName === 'BODY')) {
         if (elem.hasAttribute('data-section-id')) {
             sectionId = elem.getAttribute('data-section-id');
-            menuItemIds = elem.getAttribute('data-menu-items');
+            menuItem = elem;
             break;
         }
         elem = elem.parentNode;
     }
 
     // Get the IDs of the selected item in horizontal and vertical menus.
-    if (menuItemIds) {
-        for (let menuItemId of menuItemIds.split(" ")) {
-            menuItems.push(document.getElementById(menuItemId));
-        }
+    if (menuItem) {
         // Scroll to selected section.
         state.scrollStartedByMenuClick = true;
-        document.getElementById(sectionId).scrollIntoView({behavior: "smooth"});
+        document.getElementById(sectionId).scrollIntoView({behavior: 'smooth'});
         // Set underline at menu item in horizontal and vertical menus.
-        menuSetUnderline(menuItems);
+        menuSetUnderline(menuItem);
     }
 }
 
-function menuSetUnderline(menuItems) {
+function menuSetUnderline(menuItem) {
     // Remove underline from active menu item.
-    if (!(state.activeMenuItems === undefined || state.activeMenuItems.length === 0)) {
-        while (state.activeMenuItems.length > 0) {
-            let activeMenuItem = state.activeMenuItems.pop();
-            if (activeMenuItem.classList.contains("underlined")) {
-                activeMenuItem.classList.remove("underlined");
+    if (!(state.activeMenuItem === undefined || state.activeMenuItem.length === 0)) {
+        while (state.activeMenuItem.length > 0) {
+            let activeMenuItem = state.activeMenuItem.pop();
+            if (activeMenuItem.classList.contains('underlined')) {
+                activeMenuItem.classList.remove('underlined');
             }
-            if (activeMenuItem.classList.contains("underline-anchor")) {
-                activeMenuItem.classList.remove("underline-anchor");
+            if (activeMenuItem.classList.contains('underline-anchor')) {
+                activeMenuItem.classList.remove('underline-anchor');
             }
         }
     }
     // Add underline to selected menu items.
-    for (let menuItem of menuItems) {
-        const menuItemText = document.getElementById(menuItem.id + "-text");
-        menuItemText.classList.add("underlined");
-        menuItemText.classList.add("underline-anchor");
-        // Save selected item as active item.
-        state.activeMenuItems.push(menuItemText);
-    }
-}
-
-function* range(start, stop, step) {
-    for (let x = start; x <= stop; x = x + step) {
-        yield x;
-    }
+    const menuItemText = document.getElementById(menuItem.id + '-text');
+    menuItemText.classList.add('underlined');
+    menuItemText.classList.add('underline-anchor');
+    // Save selected item as active item.
+    state.activeMenuItem.push(menuItemText);
 }
 
 function toggleVerticalMenu() {
     // Toggle the vertical menu whenever a media query resize event fires.
-    const headerBottom = document.querySelector('#header-bottom');
-    headerBottom.classList.add('collapsed');
+    const header = document.getElementById('menu');
+    if (window.innerWidth > 950 && header.classList.contains('header__menu--display-none')) {
+        header.classList.remove('header__menu--display-none');
+    }
+    if (window.innerWidth <= 950 && !(header.classList.contains('header__menu--display-none'))) {
+        header.classList.add('header__menu--display-none');
+    }
 }
 
-function menuIconClick(elem) {
-    const headerBottom = document.querySelector('#header-bottom');
-    headerBottom.classList.toggle('collapsed');
+function menuIconClick() {
+    const header = document.getElementById('menu');
+    header.classList.toggle('header__menu--display-none');
 }
 
 function scroll() {
-    const viewWindow = document.getElementById("sections");
+    const viewWindow = document.getElementById('sections');
     const triggerDiff = 30; // Minimum scroll distance before doing something.
     const scrollTimeOut = 100;
 
@@ -127,12 +162,12 @@ function scrollingStopped() {
     // Only update the menu item underline when the user scrolls.
     // Do nothing if the scrolling happens because of .scrollIntoView().
     if (!(state.scrollStartedByMenuClick)) {
-        const viewWindow = document.getElementById("sections");
+        const viewWindow = document.getElementById('sections');
         const triggerTop = viewWindow.getBoundingClientRect().top;
         const triggerBottom = triggerTop + viewWindow.getBoundingClientRect().height * 0.3;
 
         // Find the section that is in focus.
-        const sections = document.getElementsByClassName("section");
+        const sections = document.getElementsByClassName('section');
         let focusSection = null;
         for (let section of sections) {
             if ((section.getBoundingClientRect().top >= triggerTop) &&
@@ -146,7 +181,7 @@ function scrollingStopped() {
         if (!(focusSection == null)) {
             let menuItems = [];
             let menuItemIds = focusSection.getAttribute('data-menu-items');
-            for (let menuItemId of menuItemIds.split(" ")) {
+            for (let menuItemId of menuItemIds.split(' ')) {
                 menuItems.push(document.getElementById(menuItemId));
             }
             // Set underline at menu item in horizontal and vertical menus.
